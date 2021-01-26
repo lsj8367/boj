47b1ea755b3406e5618e42802bf9d185c7a6eb69
lsj8367 <lsj8367@naver.com>
2021-01-26 17:09:53 +0900

    11720 주석 수정

--- BJAlgo/src/문자열/ex11720.java
+++ BJAlgo/src/문자열/ex11720.java
@@ -3,8 +3,8 @@ package 문자열;
 import java.util.Scanner;
 
 public class ex11720 {
-	// N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
 	public static void main(String[] args) {
+		// N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
