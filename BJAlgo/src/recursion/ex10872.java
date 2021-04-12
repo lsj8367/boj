1b80a4df546c76dc18df14435ac40c964ca89879
이승재 <lsj8367@naver.com>
2021-04-12 11:09:57 +0900

    재귀함수 팩토리얼 해결
    백준알고리즘 ex10872 완료

---
+++ BJAlgo/src/recursion/ex10872.java
@@ -0,0 +1,20 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class ex10872 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(factorial(n));
+	}
+
+	public static int factorial(int n) {
+		if(n == 0){
+			return 1;
+		}else {
+			return factorial(n - 1) * n;
+		}
+	}
+}
