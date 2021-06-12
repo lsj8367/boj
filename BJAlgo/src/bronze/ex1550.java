0aa439230e55b4b020431a5f8bef700460b9bb1f
SeungJae Lee <lsj8367@naver.com>
2021-06-13 01:17:41 +0900

    1550문제 완료

---
+++ BJAlgo/src/bronze/ex1550.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class ex1550 {
+    //16진수 입력받아 10진수 출력
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x16 = sc.next();
+
+        System.out.println(Integer.valueOf(x16, 16));
+    }
+}
