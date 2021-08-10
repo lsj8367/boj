2df12dc503a41977946b4c656344b293789cccd4
SeungJae Lee <lsj8367@naver.com>
2021-08-10 23:37:23 +0900

    fix: 변수 수정

--- BJAlgo/src/codingtest/chapter4_2.java
+++ BJAlgo/src/codingtest/chapter4_2.java
@@ -13,22 +13,18 @@ public class chapter4_2 {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x = 0;
-        int y = 0;
         // 좌표 입력 받고
         String[] a = sc.nextLine().split("");
-        int nextX = 0;
-        int nextY = 0;
-
-        x = a[0].charAt(0);
-        y = Integer.parseInt(a[1]);
+        int x = a[0].charAt(0);
+        int y = Integer.parseInt(a[1]);
         int result = 0;
 
         int[][] cases = {{-2, 1}, {-2, -1}, {-1, 2}, {-1, -2}, {1, 2}, {1, -2}, {2, 1}, {2, -1}};
         // a: 97  h: 104
         for (int[] aCase : cases) {
-            nextX = x + aCase[0];
-            nextY = y + aCase[1];
+            int nextX = x + aCase[0];
+            int nextY = y + aCase[1];
+
             if (nextX > 97 && nextX < 104 && nextY > 1 && nextY < 8) {
                 result++;
             }
