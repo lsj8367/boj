11a49565a5a4491d48e71c7f2fa7d8bb92527c01
SeungJae Lee <lsj8367@naver.com>
2021-08-10 22:22:34 +0900

    refactor: 챕터3-3으로 변경

--- BJAlgo/src/codingtest/chapter3_3.java
+++ BJAlgo/src/codingtest/chapter3_3.java
@@ -2,7 +2,7 @@ package codingtest;
 
 import java.util.Scanner;
 
-public class CardGame {
+public class chapter3_3 {
     // 숫자가 쓰인 카드들이 N x M 형태로 놓여 있다. 이때 N은 행의 개수, M은 열의 개수를 의미한다.
     // 먼저 뽑고자 하는 카드가 포함되어 있는 행을 선택한다.
     // 그 다음 선택된 행에 포함된 카드들 중 가장 숫자가 낮은 카드를 뽑아야 한다.
