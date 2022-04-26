e11e81898eb80bb72d2078f0dc1e4da0ff7616f8
lsj8367 <lsj8367@naver.com>
2022-04-26 22:39:58 +0900

    :sparkles: 2751 수 정렬하기2

---
+++ src/main/java/sort/Ex2751.java
@@ -0,0 +1,31 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Ex2751 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for (int number : list) {
+            sb.append(number).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
