e4373dde34ccf8764eb4e02a8541028b9dd8d5d6
lsj8367 <lsj8367@naver.com>
2022-05-04 22:13:47 +0900

    :sparkles: 11720 숫자의 합 다시풀이

---
+++ src/main/java/array/Ex11720.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Ex11720 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        final int[] arr = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt)
+            .toArray();
+
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
