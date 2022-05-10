6ffd23ff1aaa12ff24b57afe3b83b93bc41b8dd9
SeungJae Lee <lsj8367@naver.com>
2022-05-10 22:50:34 +0900

    슬라이싱 윈도우 풀이 (#10)
    * :sparkles: 12891 DNA비밀번호
    
    * :sparkles: 11003 최솟값 찾기

---
+++ src/main/java/slicingwindow/Ex11003.java
@@ -0,0 +1,55 @@
+package slicingwindow;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Ex11003 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        Deque<Node> deque = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            int now = Integer.parseInt(st.nextToken());
+
+            while (!deque.isEmpty() && deque.getLast().value > now) {
+                deque.removeLast();
+            }
+
+            deque.addLast(new Node(i, now));
+
+            // i가 주어진 슬라이싱 범위 L보다 커질경우 맨앞 제거
+            if (deque.getFirst().index <= i - L) {
+                deque.removeFirst();
+            }
+
+            bw.write(deque.getFirst().value + " ");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static class Node {
+        private int index;
+        private int value;
+
+        public Node(final int index, final int value) {
+            this.index = index;
+            this.value = value;
+        }
+
+    }
+}
