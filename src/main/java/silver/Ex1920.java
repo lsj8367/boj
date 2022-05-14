353b4696e69ad35a0bba585e2124e6770bcef6c8
lsj8367 <lsj8367@naver.com>
2022-05-14 17:10:53 +0900

    :sparkles: 1920 수찾기

---
+++ src/main/java/silver/Ex1920.java
@@ -0,0 +1,62 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex1920 {
+
+    private static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < m; i++) {
+            if (binarySearch(Integer.parseInt(st.nextToken())) >= 0) {
+                bw.write(1 + "\n");
+            } else {
+                bw.write(0 + "\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static int binarySearch(int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (key < arr[mid]) {
+                high = mid - 1;
+            } else if (key > arr[mid]) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
