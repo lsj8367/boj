c7911bce6dce2fee3dd3bcd102360672d758b21a
lsj8367 <lsj8367@naver.com>
2022-12-05 21:46:29 +0900

    :sparkles: 11399 ATM

---
+++ src/main/java/greedy/Ex11399.java
@@ -0,0 +1,37 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex11399 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            count += arr[i];
+            for (int j = 0; j < i; j++) {
+                count += arr[j];
+            }
+        }
+
+        System.out.println(count);
+    }
+
+}
