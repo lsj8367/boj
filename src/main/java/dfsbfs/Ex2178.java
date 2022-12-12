1b367030dcf843304302af59cf8d1eb04eb15811
lsj8367 <lsj8367@naver.com>
2022-12-12 21:39:40 +0900

    :sparkles: 2178 미로탐색 재풀이

--- src/main/java/dfsbfs/Ex2178.java
+++ src/main/java/dfsbfs/Ex2178.java
@@ -11,66 +11,55 @@ public class Ex2178 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         Queue<Pair> q = new LinkedList<>();
-        int[][] distance = new int[102][102];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < distance.length; i++) {
-            for (int j = 0; j < distance.length; j++) {
+        final int[] moveX = new int[]{1, 0, -1, 0};
+        final int[] moveY = new int[]{0, 1, 0, -1};
+
+        int[][] distance = new int[n][m];
+        int[][] maze = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 distance[i][j] = -1;
             }
         }
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        int[][] arr = new int[n + 1][m + 1];
-        int[] dx = new int[]{1, 0, -1, 0};
-        int[] dy = new int[]{0, 1, 0, -1};
-
         for (int i = 0; i < n; i++) {
-            final String[] split = br.readLine().split("");
+            String[] arr = br.readLine().split("");
             for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(split[j]);
+                maze[i][j] = Integer.parseInt(arr[j]);
             }
         }
 
         q.offer(new Pair(0, 0));
         distance[0][0] = 0;
+
         while (!q.isEmpty()) {
-            final Pair pair = q.poll();
-
+            final Pair p = q.poll();
             for (int i = 0; i < 4; i++) {
-                int newX = pair.x + dx[i];
-                int newY = pair.y + dy[i];
+                int newX = moveX[i] + p.x;
+                int newY = moveY[i] + p.y;
 
-                if (newX < 0 || newX >= n || newY < 0 || newY >= m) {
+                if (newX < 0 || newY < 0 || newX >= n || newY >= m) {
                     continue;
                 }
 
-                if (arr[newX][newY] != 1 || distance[newX][newY] >= 0) {
+                if (maze[newX][newY] != 1 || distance[newX][newY] >= 0) {
                     continue;
                 }
-
-                distance[newX][newY] = distance[pair.x][pair.y] + 1;
+                distance[newX][newY] = distance[p.x][p.y] + 1;
                 q.offer(new Pair(newX, newY));
             }
         }
 
-        //처음에 0,0으로 시작해줬기 때문에 n,m 좌표에서 -1씩 빼주고 +1 (마지막거리 더함)
         System.out.println(distance[n - 1][m - 1] + 1);
     }
 
-    private static class Pair {
-
-        private final int x;
-        private final int y;
-
-        public Pair(final int x, final int y) {
-            this.x = x;
-            this.y = y;
-        }
-
+    private record Pair(int x, int y) {
     }
 
 }
